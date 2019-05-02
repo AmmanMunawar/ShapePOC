58467501eec4922e9dedae94a4228155e921779d
unknown <Aman Munawar@DESKTOP-71N88GO.ebricks-inc.com>
2019-05-02 07:50:20 -0700

    I created java objects through json

--- src/main/java/com/ebricks/shape/Circle.java
+++ src/main/java/com/ebricks/shape/Circle.java
@@ -7,8 +7,32 @@ class Circle extends Shape{
     private int x;
     private int y;
     private int radius;
-    //creating logger objects\
     private static final Logger LOGGER = LogManager.getLogger(Circle.class.getName());
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    //creating logger objects\
+    public Circle(){
+
+    }
     public Circle(int x,int y ,int radius){
         this.x = x;
         this.y = y;
--- src/main/java/com/ebricks/shape/Main.java
+++ src/main/java/com/ebricks/shape/Main.java
@@ -1,23 +1,36 @@
 package com.ebricks.shape;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
 
 public class Main {
 
     private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
-    public static void main(String[] args) {
-//        Circle circle_object = new Circle(1,2,3);
-        Shape shape = new Circle(1,2,3);
-        shape.draw();
-        shape = new Rectangle(2,1,3,2);
-        shape.draw();
+    public static void main(String[] args){
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+//        String string = "{/"x"/:1,/"y"/:2,/"radius"/:3}";
+            String jsonforcircle = "{ \"x\" : 12, \"y\" : 32,\"radius\" : 34 }";
+            Shape shape = objectMapper.readValue(jsonforcircle, Circle.class); //circle
+            shape.draw();
+            String jsonforrectangle = "{ \"x\" : 12, \"y\" : 32,\"width\" : 34,\"height\":23 }";
+            shape = objectMapper.readValue(jsonforrectangle, Rectangle.class);
+            shape.draw();
 
 
-        //these are main class logger
-        System.out.println("Hello world!!");
-        LOGGER.debug("Logged in Main Class Debug !!!");
-
+            //these are main class logger
+            System.out.println("Hello world!!");
+            LOGGER.debug("Logged in Main Class Debug !!!");
+        }
+        catch (JsonParseException e) { e.printStackTrace();}
+        catch (JsonMappingException e) { e.printStackTrace(); }
+        catch (IOException e) { e.printStackTrace(); }
 
     }
 
