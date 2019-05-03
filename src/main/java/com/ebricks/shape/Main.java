e6af604106f246446ea1e937f4aa90ac56b2d10f
unknown <Aman Munawar@DESKTOP-71N88GO.ebricks-inc.com>
2019-05-03 06:36:38 -0700

    Json to java object through file

--- src/main/java/com/ebricks/shape/Main.java
+++ src/main/java/com/ebricks/shape/Main.java
@@ -1,37 +1,33 @@
 package com.ebricks.shape;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-    private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
-    public static void main(String[] args){
+        ObjectMapper objectMapper = new ObjectMapper();
+        Canvas canvas = objectMapper.readValue(new File("jsonfile.json"), Canvas.class);
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        for (final Shape shape : canvas.getShapeList()) {
+                executor.execute(new Runnable() {
+                public void run() {
+                    shape.draw();
 
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-//        String string = "{/"x"/:1,/"y"/:2,/"radius"/:3}";
-            String jsonforcircle = "{ \"x\" : 12, \"y\" : 32,\"radius\" : 34 }";
-            Shape shape = objectMapper.readValue(jsonforcircle, Circle.class); //circle
-            shape.draw();
-            String jsonforrectangle = "{ \"x\" : 12, \"y\" : 32,\"width\" : 34,\"height\":23 }";
-            shape = objectMapper.readValue(jsonforrectangle, Rectangle.class);
-            shape.draw();
-
-
-            //these are main class logger
-            System.out.println("Hello world!!");
-            LOGGER.debug("Logged in Main Class Debug !!!");
+//                    System.out.println("Hello world");
+                }
+            });
         }
-        catch (JsonParseException e) { e.printStackTrace();}
-        catch (JsonMappingException e) { e.printStackTrace(); }
-        catch (IOException e) { e.printStackTrace(); }
-
+        executor.shutdown();
     }
-
 }
