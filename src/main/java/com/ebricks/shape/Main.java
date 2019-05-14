ef8d2184c89e068066722a0be2a2960381e65771
Aman Munawar <aman.munawar@ebicks-inc.com>
2019-05-14 00:08:01 -0700

    I have added configuration package and its class with some more changes in HttpClient Get Post methods

--- src/main/java/com/ebricks/shape/Main.java
+++ src/main/java/com/ebricks/shape/Main.java
@@ -1,6 +1,5 @@
 package com.ebricks.shape;
 
-import com.ebricks.shape.connectserver.HttpURLConnectionServlet;
 import com.ebricks.shape.processor.ShapeProcessor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,12 +8,8 @@ import java.io.IOException;
 public class Main {
 
     private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
-    private static HttpURLConnectionServlet httpURLConnectionServlet= new HttpURLConnectionServlet();
 
     public static void main(String[] args) throws IOException {
-
-        httpURLConnectionServlet.sendPOST();
-        httpURLConnectionServlet.sendGET();
 
         ShapeProcessor shapeProcessor = new ShapeProcessor();
         try {
--- src/main/java/com/ebricks/shape/model/Canvas.java
+++ src/main/java/com/ebricks/shape/model/Canvas.java
@@ -1,8 +1,8 @@
 package com.ebricks.shape.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Canvas {
     private List<Shape> shapeList;
